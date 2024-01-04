0c8945e72fe3c87cb7fcac773d0e215cbd19bf73
Adnan <155667335+Adnan-CAS@users.noreply.github.com>
2024-01-05 03:44:52 +0800

    Add files via upload

---
+++ SEGA-GPU-Version/paths/paths.py
@@ -0,0 +1,42 @@
+import os
+import pathlib
+
+data_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/datasets") # TODO
+
+### Data Paths ###
+sega_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/datasets/SEGA")
+
+### RAW Data Paths ###
+raw_sega_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/datasets/SEGA/RAW/")
+
+### Parsed Data Paths ###
+parsed_sega_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/datasets/SEGA/PARSED")
+
+### Training Paths ###
+project_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src") # TODO
+checkpoints_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/Checkpoints/")
+logs_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/Logs")
+figures_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/Figures")
+models_path = (r"C:/ProgramData/anaconda3/envs/SEGA-Latest/SEGA-2023/src/Models")
+
+
+# import os
+# import pathlib
+
+# data_path = None # TODO
+
+# ### Data Paths ###
+# sega_path = data_path / "SEGA"
+
+# ### RAW Data Paths ###
+# raw_sega_path = data_path / "SEGA" / "RAW"
+
+# ### Parsed Data Paths ###
+# parsed_sega_path = data_path / "SEGA" / "PARSED"
+
+# ### Training Paths ###
+# project_path = None # TODO
+# checkpoints_path = project_path / "Checkpoints"
+# logs_path = project_path / "Logs"
+# figures_path = project_path / "Figures"
+# models_path = project_path / "Models"
